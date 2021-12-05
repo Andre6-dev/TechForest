3ad7c2a14d42e7b39be3faeea3ce043d70f9438d
Nikoll Bonilla <nikoll.bonilla@tecsup.edu.pe>
2021-12-05 12:48:15 -0500

    Andre

--- techForest/app/views.py
+++ techForest/app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from api.models import *
 
 # Pagina principal
 def index(request):
@@ -22,24 +23,57 @@ def contacto(request):
     return render(request, 'principal/contacto.html', context)
 
 # Dashboard Cliente
-def cliente(request):
-    context = {}
-    return render(request, 'cliente/index.html', context)
+def cliente(request, cliente_id):
+    cliente = get_object_or_404(Usuarios, pk=cliente_id)
+    plan = Planes.objects.get(usuarios_id=cliente_id)
+    return render(request, 'cliente/index.html', {'cliente': cliente,'plan': plan})
 
-def perfilCliente(request):
-    context = {}
-    return render(request, 'cliente/perfil.html', context)
+def perfilCliente(request, cliente_id):
+    cliente = get_object_or_404(Usuarios, pk=cliente_id)
+    return render(request, 'cliente/perfil.html', {'cliente': cliente})
 
-def planCliente(request):
-    context = {}
-    return render(request, 'cliente/plan.html', context)
+def planCliente(request, cliente_id):
+    cliente = get_object_or_404(Usuarios, pk=cliente_id)
+    plan = Planes.objects.get(usuarios_id=cliente_id)
+    pagos = Pagos.objects.filter(planes_id=plan.id)
+    return render(request, 'cliente/plan.html', {'cliente': cliente,'plan': plan,'pagos': pagos})
 
-def solucionCliente(request):
-    context = {}
-    return render(request, 'cliente/solucion.html', context)
+def solucionCliente(request, cliente_id):
+    cliente = get_object_or_404(Usuarios, pk=cliente_id)
+    diapositivo = Diapositivos.objects.get(usuarios_id=cliente_id)
+    opciones = Opciones.objects.get(diapositivos_id=diapositivo.id)
+    return render(request, 'cliente/solucion.html', {'cliente': cliente,'opciones': opciones})
 
-def contactoCliente(request):
-    context = {}
-    return render(request, 'cliente/contacto.html', context)
+def reportarCliente(request, cliente_id):
+    cliente = get_object_or_404(Usuarios, pk=cliente_id)
+    return render(request, 'cliente/reportar.html', {'cliente': cliente})
+
+def reportesCliente(request, cliente_id):
+    cliente = get_object_or_404(Usuarios, pk=cliente_id)
+    errores = Errores.objects.filter(usuarios_id=cliente.id)
+    return render(request, 'cliente/reportes.html', {'cliente': cliente,'errores': errores})
 
 # Dashboard Administrador
+def admin_dashboard(request):
+    context = {}
+    return render(request, 'administrador/admin_dashboard.html', context)
+
+
+def administradores(request):
+    context = {}
+    return render(request, 'administrador/administradores.html', context)
+
+
+def ayuda(request):
+    context = {}
+    return render(request, 'administrador/ayuda.html', context)
+
+
+def dispositivos(request):
+    context = {}
+    return render(request, 'administrador/dispositivos.html', context)
+
+
+def usuarios(request):
+    context = {}
+    return render(request, 'administrador/usuarios.html', context)
